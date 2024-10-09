b1e172f8e32605b4cc55908490673b2cc53d63ad
coolshou <jimmy_yeh@alphanetworks.com>
2024-10-09 08:50:48 +0800

    add debian files

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/murt/issues
+# Bug-Submit: https://github.com/<user>/murt/issues/new
+# Changelog: https://github.com/<user>/murt/blob/master/CHANGES
+# Documentation: https://github.com/<user>/murt/wiki
+# Repository-Browse: https://github.com/<user>/murt
+# Repository: https://github.com/<user>/murt.git
